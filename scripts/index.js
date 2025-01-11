0ed49741368f98743c1c0e15d8781b6dd0232a14
Lullalei <pearlborromeo9@gmail.com>
2025-01-11 15:00:34 -0700

    (feature) added Create Event button

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 import { initViewSelect } from "./view-select.js";
+import { initEventCreateButton } from "./event-create-button.js";
+import { initEventFormDialog } from "./event-form-dialog.js";
 import { initCalendar } from "./calendar.js";
 
 initViewSelect();
+initEventCreateButton();
+initEventFormDialog();
 initCalendar();
